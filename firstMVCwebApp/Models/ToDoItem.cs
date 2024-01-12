30f4c6c6bcb6b037281c80dc295d4ee2fe643b01
adamstgr <adam.stgr@gmail.com>
2024-01-11 22:00:07 -0500

    Adds ToDoItemController, ToDoItem model and the view for creating a ToDoItem and adds the creation to the header bar

---
+++ firstMVCwebApp/Models/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿namespace firstMVCwebApp.Models
+{
+    public class ToDoItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime? DoItemWhen {  get; set; }
+        public bool Reminder { get; set; }
+
+        public ToDoItem() 
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
